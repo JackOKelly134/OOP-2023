c333681dadf0fdc723f174d29907555907e1b76c
Jack O'Kelly <c21461876@mytudublin.ie>
2023-02-22 23:06:15 +0000

    lab5

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -6,7 +6,7 @@ import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import processing.core.PApplet;
 
-public class audio1 extends PApplet
+public class Audio1 extends PApplet
 {
 
     Minim minim;
@@ -61,9 +61,4 @@ public class audio1 extends PApplet
 	}
 
     float lerpedR = 0;
-
-    public void keyPressed()
-    {
-        
-    }
 }
